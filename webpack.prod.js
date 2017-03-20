8a4d1d14d76de2795be66e4ad256ee2a017523a1
eareese <eareese@eareese.com>
2017-03-20 09:46:02 -0400

    Add webpack production config

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const webpackMerge = require('webpack-merge'),
+      commonConfig = require('./webpack.config.js'),
+      path = require('path')
+
+module.exports = function(env) {
+  return webpackMerge(commonConfig(), {
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[chunkhash].js'
+    }
+  })
+}
